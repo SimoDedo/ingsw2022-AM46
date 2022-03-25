df9d6f022c01b0c6a7de04dc773d0dbd784199c3
dedon <simone.dedonato@mail.polimi.it>
2022-03-25 13:38:05 +0100

    Specified Color an TowerColor enums

--- src/main/java/it/polimi/ingsw/Utils/Enum/Color.java
+++ src/main/java/it/polimi/ingsw/Utils/Enum/Color.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.Utils.Enum;
 
+/**
+ * Enum to model different Student,Professor and Table colors
+ */
 public enum Color {
+    YELLOW,
+    BLUE,
+    GREEN,
+    RED,
+    PINK
 }
--- src/main/java/it/polimi/ingsw/Utils/Enum/TowerColor.java
+++ src/main/java/it/polimi/ingsw/Utils/Enum/TowerColor.java
@@ -1,4 +1,10 @@
 package it.polimi.ingsw.Utils.Enum;
 
+/**
+ * Enum to model different Tower colors
+ */
 public enum TowerColor {
+    BLACK,
+    WHITE,
+    GREY
 }
