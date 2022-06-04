d48a52ee40aa96f3c00fe57aa7647aefb597fbc2
Gregory Dimaglie <gregorio.dimaglie@mail.polimi.it>
2022-06-05 01:26:16 +0200

    Fixed position of color selection box in character

--- src/main/java/it/polimi/ingsw/View/GUI/Application/ColorSelectionPane.java
+++ src/main/java/it/polimi/ingsw/View/GUI/Application/ColorSelectionPane.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.View.GUI.Application;
 
 import it.polimi.ingsw.Utils.Enum.Color;
+import javafx.geometry.Insets;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -10,9 +11,12 @@ import java.util.Map;
 
 public class ColorSelectionPane extends HBox {
 
+    public static double colorSize = 25.0;
+
     public static Map<Color, Integer> colorOrder;
 
     public ColorSelectionPane(int charID) {
+        this.setPadding(new Insets(50.0, 5.0, 5.0, 5.0));
         this.setId("char" + charID + "ColorPane");
         this.setVisible(false);
 
@@ -23,9 +27,9 @@ public class ColorSelectionPane extends HBox {
             colorView.setId("char" + charID + "Color" + color.toString().toLowerCase());
             Image colorImage = new Image("/chars/" + color.toString().toLowerCase() + ".png");
             colorView.setImage(colorImage);
-            colorView.setEffect(new DropShadow(50.0/2.0, javafx.scene.paint.Color.BLACK));
+            colorView.setEffect(new DropShadow(colorSize/2.0, javafx.scene.paint.Color.BLACK));
             colorView.setPreserveRatio(true);
-            colorView.setFitHeight(50.0);
+            colorView.setFitHeight(colorSize);
             colorView.setSmooth(true);
             colorView.setCache(true);
             colorView.setPickOnBounds(false);
