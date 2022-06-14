94a2b82dadd829d02be229a9cf88e208f3820331
dedon <simone.dedonato@mail.polimi.it>
2022-06-14 13:51:23 +0200

    Fixed bug that wouldn't allow you to move mother nature to the same island that was occupied even if move count was enough to make a full loop; added test for this edge case

--- src/test/java/it/polimi/ingsw/GameModel/Board/Archipelago/MoveMotherNatureStrategy/MoveMotherNatureStrategyTest.java
+++ src/test/java/it/polimi/ingsw/GameModel/Board/Archipelago/MoveMotherNatureStrategy/MoveMotherNatureStrategyTest.java
@@ -30,9 +30,29 @@ class MoveMotherNatureStrategyTest {
         Archipelago archipelago = new Archipelago();
 
         int idxStartIG = archipelago.getMotherNatureIslandGroupIndex();
-        int idxEndIG =  idxStartIG + 6 > 11 ? idxStartIG + 6 - 12 : idxStartIG + 6;
         int moveCount = 6;
+        int idxEndIG =  idxStartIG + moveCount > 11 ? idxStartIG + moveCount - 12 : idxStartIG + moveCount;
         archipelago.moveMotherNature(archipelago.getIslandTilesIDs().get(idxEndIG).get(0), moveCount);
         assertEquals(archipelago.getMotherNatureIslandGroupIndex(), idxEndIG, "unexpected mother nature location index after move");
+    }
+
+    /**
+     * Tests that method correctly lets player move MotherNature when making an entire loop if capable
+     */
+    @Test
+    void moveMotherNatureLoop() {
+        Archipelago archipelago = new Archipelago();
+
+        int idxStartIG = archipelago.getMotherNatureIslandGroupIndex();
+        int moveCount = 12;
+        int idxEndIG =  idxStartIG + moveCount > 11 ? idxStartIG + moveCount - 12 : idxStartIG + moveCount;
+        archipelago.moveMotherNature(archipelago.getIslandTilesIDs().get(idxEndIG).get(0), moveCount);
+        assertEquals(archipelago.getMotherNatureIslandGroupIndex(), idxEndIG, "unexpected mother nature location index after move");
+
+        int idxStartIG2 = archipelago.getMotherNatureIslandGroupIndex();
+        int moveCount2 = 2;
+        int idxEndIG2 =  idxStartIG2 + 12 > 11 ? idxStartIG2 + 12 - 12 : idxStartIG2 + 12;
+        assertThrows(IllegalArgumentException.class,() -> archipelago.moveMotherNature(archipelago.getIslandTilesIDs().get(idxEndIG2).get(0), moveCount2),
+                "no exception raised on illegal mother nature movement");
     }
 }
